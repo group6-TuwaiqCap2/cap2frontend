90028fe716f656eca930614c33ee0629f8582b62
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-11-11 11:59:21 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Routes, Route} from "react-router-dom"
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Footer />
+
+      <Routes>
+        <Route exact path="/" component={Home} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
