dcebfc19503f1f80df740794d301f64a3f2931fd
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-11-12 23:45:44 +0300

    ebook component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
-import Movie from "./components/Movie";
-import TvShow from "./components/TvShow";
 import Audiobook from "./components/Audiobook";
+import Ebook from "./components/Ebook";
+
+import Movie from "./components/Movie";
 
 const App = () => {
   return (
@@ -17,7 +18,7 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/movie" element={<Movie />} />
         <Route exact path="/audiobook" element={<Audiobook />} />
-        {/* <Route exact path="/" element={Home} /> */}
+        <Route exact path="/ebook" element={<Ebook/>} />
         {/* <Route exact path="/" element={Home} /> */}
         {/* <Route exact path="/" element={Home} /> */}
         {/* <Route exact path="/" element={Home} /> */}
--- src/components/Ebook/index.js
+++ src/components/Ebook/index.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Ebook = () => {
+  const BASE_URL = "http://localhost:4000";
 
+  const [ebooks, setEbook] = useState([]);
+  useEffect(() => {
+    getAllebook();
+  }, []);
+  const getAllebook = async () => {
+    const ebooks = await axios.get(`${BASE_URL}/ebook`);
+    console.log(ebooks);
+    setEbook(ebooks.data.results);
+  };
   return (
-    <div>
-      <h1>Ebook</h1>
+    <div className="App">
+      <h1> Ebook </h1>
+      <ul>
+        {ebooks.map((ebook) => (
+          <li key={ebook.trackId}>
+            {ebook.trackName}
+            <img src={ebook.artworkUrl100}></img>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Ebook;
