8511b187875d638fad7daa91286b4fda5f269de0
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-11-13 00:07:39 +0300

    musicVidio component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import style from "./App.css";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Routes, Route } from "react-router-dom";
 import Audiobook from "./components/Audiobook";
 import Ebook from "./components/Ebook";
 import Music from "./components/Music";
+import MusicVideo from "./components/MusicVideo";
 
 import Movie from "./components/Movie";
 
@@ -21,7 +21,7 @@ const App = () => {
         <Route exact path="/audiobook" element={<Audiobook />} />
         <Route exact path="/ebook" element={<Ebook/>} />
         <Route exact path="/music" element={<Music />} />
-        {/* <Route exact path="/" element={Home} /> */}
+        <Route exact path="/musicVideo" element={<MusicVideo />} />
         {/* <Route exact path="/" element={Home} /> */}
       </Routes>
       <Footer />
--- src/components/MusicVideo/index.js
+++ src/components/MusicVideo/index.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const MusicVideo = () => {
+  const BASE_URL = "http://localhost:4000";
 
+  const [musicVideos, setmusicVideos] = useState([]);
+  useEffect(() => {
+    getAllMusicVideo();
+  }, []);
+  const getAllMusicVideo = async () => {
+    const musicVideos = await axios.get(`${BASE_URL}/musicVideo`);
+    console.log(musicVideos);
+    setmusicVideos(musicVideos.data.results);
+  };
   return (
-    <div>
-      <h1>MusicVideo</h1>
+    <div className="App">
+      <h1> Ebook </h1>
+      <ul>
+        {musicVideos.map((musicVideos) => (
+          <li key={musicVideos.trackId}>
+            {musicVideos.trackName}
+            <img src={musicVideos.artworkUrl100}></img>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default MusicVideo;
