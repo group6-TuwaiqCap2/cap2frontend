a04b1f5284c80b8d322b26d09777fb8652938530
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-11-12 14:41:55 +0300

    inintial navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,49 @@
-import React from 'react'
-
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import "./style.css";
 const Nav = () => {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
-  )
-}
+    <>
+      <div className="wrapper">
+        <NavLink to="/" className="links">
+          <h1>logo</h1>
+        </NavLink>
+        <div className="navMenu">
+          <NavLink to="/movie" className="links">
+            Movie
+          </NavLink>
+          <NavLink to="/podcast" className="links">
+            Podcast
+          </NavLink>
+          <NavLink to="/music" className="links">
+            Music
+          </NavLink>
+          <NavLink to="/musicVideo" className="links">
+            Music Video
+          </NavLink>
+          <NavLink to="/audiobook" className="links">
+            Audiobook
+          </NavLink>
+          <NavLink to="/tvShow" className="links">
+            Tv Show
+          </NavLink>
+          <NavLink to="/software" className="links">
+            Software
+          </NavLink>
+          <NavLink to="/ebook" className="links">
+            Ebook
+          </NavLink>
+        </div>
+        <button className="btn">=</button>
+
+        <button className="navBtn">
+          <Link to="signin" className="navBtnLink">
+            Sign In
+          </Link>
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Nav;
