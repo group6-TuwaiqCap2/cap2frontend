08e63d456aae3ddd285bd214b5c26a8835a9d4cd
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-11-13 11:21:29 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from "react";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import React {Routes, Route} from "react-router-dom";
+import  {Routes, Route} from "react-router-dom";
 import style from "./App.css";
 
 const App = () => {
