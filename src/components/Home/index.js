9653438bc726ce3594111ad6ea2372d3022dd291
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-11-14 01:55:55 +0300

    create title and link

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,71 +1,91 @@
 import React from "react";
 import style from "./style.css";
 import Header from "../Header";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <h1 className="lg">𝕀𝕋𝕌ℕ𝔼𝕊</h1>
+      <h3 className="itunesTit">“Music is the universal language of mankind.” </h3>
+      <h3 style={{fontSize: "15px"}} className="itunesTit">Henry Wadsworth Longfellow</h3>
       <div className="home">
         <div className="card">
-          <img className="image" src="./img/ebook.JPG"></img>
-          <div className="content">
-            <h1>ebook</h1>
-          </div>
+          <NavLink to="/movie">
+            <img className="image" src="./img/ebook.JPG"></img>
+            <div className="content">
+              <h1>ebook</h1>
+            </div>
+          </NavLink>
         </div>
         <div className="card">
-          <img
-            className="image"
-            src="./img/audiobook.JPG"
-            style={{ height: "100%" }}
-          ></img>
-          <div className="content">
-            <h1>audiobook</h1>
-          </div>
+          <NavLink to="/audiobook">
+            <img
+              className="image"
+              src="./img/audiobook.JPG"
+              style={{ height: "100%" }}
+            ></img>
+            <div className="content">
+              <h1>audiobook</h1>
+            </div>
+          </NavLink>
         </div>
         <div className="card">
-          <img className="image" src="./img/movie.JPG"></img>
-          <div className="content">
-            <h1>movie</h1>
-          </div>
+          <NavLink to="/movie">
+            <img className="image" src="./img/movie.JPG"></img>
+            <div className="content">
+              <h1>movie</h1>
+            </div>
+          </NavLink>
         </div>
         <div className="card">
-          <img
-            className="image"
-            src="./img/music.JPG"
-            style={{ height: "100%" }}
-          ></img>
-          <div className="content">
-            <h1>music</h1>
-          </div>
+          <NavLink to="/movie">
+            <img
+              className="image"
+              src="./img/music.JPG"
+              style={{ height: "100%" }}
+            ></img>
+            <div className="content">
+              <h1>music</h1>
+            </div>
+          </NavLink>
         </div>
         <div className="card">
-          <img className="image" src="./img/musicVideo.JPG"></img>
-          <div className="content">
-            <h1>musicVideo</h1>
-          </div>
+          <NavLink to="/musicVideo">
+            <img className="image" src="./img/musicVideo.JPG"></img>
+            <div className="content">
+              <h1>musicVideo</h1>
+            </div>
+          </NavLink>
         </div>
         <div className="card">
-          <img className="image" src="./img/podcast.JPG"></img>
-          <div className="content">
-            <h1>podcast</h1>
-          </div>
+          <NavLink to="/podcast">
+            <img className="image" src="./img/podcast.JPG"></img>
+            <div className="content">
+              <h1>podcast</h1>
+            </div>
+          </NavLink>
         </div>
         <div className="card">
-          <img
-            className="image"
-            src="./img/software.JPG"
-            style={{ height: "100%" }}
-          ></img>
-          <div className="content">
-            <h1>software</h1>
-          </div>
+          <NavLink to="/software">
+            <img
+              className="image"
+              src="./img/software.JPG"
+              style={{ height: "100%" }}
+            ></img>
+            <div className="content">
+              <h1>software</h1>
+            </div>
+          </NavLink>
         </div>
         <div className="card">
-          <img className="image" src="./img/tvShow.JPG"></img>
-          <div className="content">
-            <h1>tvShow</h1>
-          </div>
+          <NavLink to="/tvShow">
+            <img className="image" src="./img/tvShow.JPG"></img>
+            <div className="content">
+              <h1>tvShow</h1>
+            </div>
+          </NavLink>
         </div>
       </div>
     </>
